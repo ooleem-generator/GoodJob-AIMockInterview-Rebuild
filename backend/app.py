f94fcd31af16184a345e6b1fcee8abbc8f7fb773
ooleem-generator <swj7563@gmail.com>
2025-12-11 15:19:57 +0900

    [Setting] Backend setting (In progress)

--- backend/app.py
+++
@@ -1,9 +0,0 @@
-# app.py
-
-from typing import Union
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-    
---
+++ backend/src/app.py
@@ -0,0 +1,22 @@
+# app.py
+
+import os
+from typing import Union
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.config import settings
+from pydantic import BaseModel
+
+# class ChatRequest(BaseModel):
+#     prompt: str # We expect a JSON body like {"prompt": "..."}
+
+# class ChatResponse(BaseModel):
+#     response: str # We'll return a JSON body like {"response": "..."}
+
+app = FastAPI()
+
+# CORS setting
+# app.add_middleware(CORSMiddleware, allow_origins=settings.CORS_ORIGINS)
+
+
+# @app.get("/")
